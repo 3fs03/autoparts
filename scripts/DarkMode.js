d57119c7482535c7305d56a1592590bc618313de
lordkroos12 <joroo.23@abv.bg>
2023-01-11 18:52:45 +0200

    dark-light mode

--- scripts/DarkMode.js
+++ scripts/DarkMode.js
@@ -1,39 +1,44 @@
-const toggle = document.getElementById('toggleDark');
-const body = document.querySelector('body');
-const line = Array.from( document.getElementsByClassName('line'));
-    if(localStorage.getItem("body") === 'dark'){
-        
-    }
-    else{
-        body.classList.toggle('active');
-        line.forEach(element => {
-            element.classList.toggle('active');
-            
-           });   
-    }
+const toggleBtn = document.getElementById("toggle-btn");
+const theme = document.querySelector("body");
+const line = Array.from(document.getElementsByClassName("line"));
+let darkMode = localStorage.getItem("dark-mode");
 
-toggle.addEventListener('click',function(){
-    if(localStorage.getItem("body") === 'light'){
-        this.removeAttribute('class');
-        this.classList.add('fa-solid','fa-sun');
-        console.log('light');
-        body.classList.toggle('active');
-        line.forEach(element => {
-            element.classList.toggle('active');
-            
-           });
-        localStorage.setItem("body","dark");
-        
-    }
-    else{
-        this.removeAttribute('class');
-        this.classList.add('fa-solid','fa-moon');
-        console.log('dark');
-        body.classList.toggle('active');
-        line.forEach(element => {
-            element.classList.toggle('active');
-            
-           });
-           localStorage.setItem("body","light");
-    }
-})
+if(darkMode === 'enabled'){
+    toggleBtn.classList.add("fa-sun");
+}
+else{
+    toggleBtn.classList.add("fa-moon");
+}
+
+const enableDarkMode = () => {
+  theme.classList.add("active");
+  line.forEach(element => {
+    element.classList.toggle('active');
+   });
+  toggleBtn.classList.remove("fa-moon");
+  toggleBtn.classList.add("fa-sun");
+  localStorage.setItem("dark-mode", "enabled");
+};
+
+const disableDarkMode = () => {
+  theme.classList.remove("active");
+  line.forEach(element => {
+    element.classList.remove('active');
+   });
+  toggleBtn.classList.remove("fa-sun");
+  toggleBtn.classList.add("fa-moon");
+  localStorage.setItem("dark-mode", "disabled");
+};
+
+if (darkMode === "enabled") {
+  enableDarkMode(); // set state of darkMode on page load
+}
+
+toggleBtn.addEventListener("click", (e) => {
+  darkMode = localStorage.getItem("dark-mode"); // update darkMode when clicked
+  if (darkMode === "disabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
