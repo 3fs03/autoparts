a918067e066bd79e3ff00acd6b61b056b8655758
lordkroos12 <joroo.23@abv.bg>
2023-01-11 18:13:51 +0200

    Update DarkMode.js

--- scripts/DarkMode.js
+++ scripts/DarkMode.js
@@ -1,20 +1,39 @@
 const toggle = document.getElementById('toggleDark');
 const body = document.querySelector('body');
 const line = Array.from( document.getElementsByClassName('line'));
-
-toggle.addEventListener('click',function(){
-    this.classList.toggle('fa-moon');
-    if(this.classList.toggle('fa-sun')){
-        body.classList.toggle('active');
-       line.forEach(element => {
-        element.classList.toggle('active');
-       });
+    if(localStorage.getItem("body") === 'dark'){
         
     }
     else{
         body.classList.toggle('active');
         line.forEach(element => {
             element.classList.toggle('active');
+            
+           });   
+    }
+
+toggle.addEventListener('click',function(){
+    if(localStorage.getItem("body") === 'light'){
+        this.removeAttribute('class');
+        this.classList.add('fa-solid','fa-sun');
+        console.log('light');
+        body.classList.toggle('active');
+        line.forEach(element => {
+            element.classList.toggle('active');
+            
            });
+        localStorage.setItem("body","dark");
+        
+    }
+    else{
+        this.removeAttribute('class');
+        this.classList.add('fa-solid','fa-moon');
+        console.log('dark');
+        body.classList.toggle('active');
+        line.forEach(element => {
+            element.classList.toggle('active');
+            
+           });
+           localStorage.setItem("body","light");
     }
 })
